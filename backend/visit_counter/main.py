c2b82c032ada35e069c1daf8c8e0e456eff67ead
Gotyee <86921217+Gotyee@users.noreply.github.com>
2024-09-03 14:40:35 +0200

    changed main path

--- backend/visit_counter/main.py
+++ backend/visit_counter/main.py
@@ -1,9 +1,8 @@
 import azure.functions as func
 
-from .visit_counter import visit_counter_app
+from . import visit_counter_app
 
 app = func.AsgiFunctionApp(
     app=visit_counter_app,
     http_auth_level=func.AuthLevel.ANONYMOUS,
 )
-e = 2
