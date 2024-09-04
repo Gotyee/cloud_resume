b5a2b81d2db2c27e743f2d1fe843364077f10849
Gotyee <86921217+Gotyee@users.noreply.github.com>
2024-09-04 21:20:17 +0200

    fixed typo

--- backend/visit_counter/function_app.py
+++ backend/visit_counter/function_app.py
@@ -1,6 +1,6 @@
 import azure.functions as func
-from azure.cosmos import CosmosClient, exceptions
-from fastapi import APIRouter, FastAPI
+from azure.cosmos import CosmosClient
+from fastapi import APIRouter, FastAPI, exceptions
 
 fast_app = FastAPI(root_path="/visit_counter")
 router = APIRouter(prefix="/api", tags=["HmmIMightBeAPI"])
@@ -11,13 +11,13 @@ container_id = "cloud_resume_db"
 database_id = "cloud_resume_db_id"
 key = "jPyFiP7XYEbZYhLCh5PaJYeP0j7yjndhJ6XjOlRTzakwYKZRPlaYK77tvEY3EKVdZZxyT5vIQlHDACDbFDWrHg=="
 partition_key = "/id"
-client = CosmosClient(url=endpoint, credentials=key)
+client = CosmosClient(url=endpoint, credential=key)
 database = client.get_database_client(database_id)
 container = database.get_container_client(container_id)
 
 
 @router.get("/visits")
-async def read_visits():
+async def read_visits() -> str:
     try:
         return container.read_item("visit_counter", partition_key=partition_key)
     except exceptions.CosmosResourceNotFoundError:
